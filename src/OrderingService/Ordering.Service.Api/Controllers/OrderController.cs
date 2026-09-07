ee39c76b337c5f9d316f37fde00d859ae2522fcb
marco08 <marcolinho1991@gmail.com>
2026-09-07 20:47:15 +0200

    Add OrderController with CreateOrder endpoint and routing attributes

--- src/OrderingService/Ordering.Service.Api/Controllers/OrderController.cs
+++ src/OrderingService/Ordering.Service.Api/Controllers/OrderController.cs
@@ -1,6 +1,17 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Service.Application.Orders;
+
 namespace Ordering.Service.Api.Controllers;
 
-public class OrderController
+[ApiController]
+[Route("api/[controller]")]
+public class OrderController(IMediator mediator) : ControllerBase
 {
-    
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder(CreateOrderCommand command, CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(command, cancellationToken);
+        return CreatedAtAction(nameof(CreateOrder), new { id = result }, result);
+    }
 }
